e05b790c93bb9499fe0e6cff08b41d3eb70bdeff
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-12 08:08:31 +0000

    automatically backed up by learn

--- db/migrate/20171212080711_create_movies.rb
+++ db/migrate/20171212080711_create_movies.rb
@@ -1,4 +1,10 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movies do |movie|
+      movie.string :title
+      movie.datetime :release_date
+      movie.string :director
+      movie.string :lead
+      movie.boolean :in_theaters
   end
 end
